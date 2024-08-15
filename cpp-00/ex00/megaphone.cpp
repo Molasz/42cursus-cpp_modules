e9f78919c3427ab1ec4412c9d22cf5236d5db1b6
Molasz <martiolasz@gmail.com>
2024-08-15 14:03:22 +0200

    cpp module 00 ex00 done

---
+++ cpp-00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molasz-a <molasz-a@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 13:35:28 by molasz-a          #+#    #+#             */
+/*   Updated: 2024/08/15 13:46:42 by molasz-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char) toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
