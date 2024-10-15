4854524b5b747fe1bb3d30fd7140c90fd3a9498b
Molasz <martiolasz@gmail.com>
2024-10-15 17:57:42 +0200

    cpp07 setup ex00 done

---
+++ cpp-07/ex00/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molasz-a <molasz-a@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 17:38:10 by molasz-a          #+#    #+#             */
+/*   Updated: 2024/10/15 17:56:24 by molasz-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main(void)
+{
+	int		a = 5, b = 42;
+	char	c = 'c', d = 'd';
+
+	std::cout << "INT Min [" << a << ", " << b << "] " << min(a,b) << std::endl;
+	std::cout << "CHAR Min [" << c << ", " << d << "] " << min(c,d) << std::endl;
+	std::cout << "INT* Min [" << &a << ", " << &b << "] " << min(&a, &b) << std::endl;
+
+	std::cout << "INT Max [" << a << ", " << b << "] " << max(a,b) << std::endl;
+	std::cout << "CHAR Max [" << c << ", " << d << "] " << max(c,d) << std::endl;
+	std::cout << "INT* Max [" << &a << ", " << &b << "] " << max(&a, &b) << std::endl;
+
+	std::cout << "A: " << a << " B: " << b << std::endl;
+	swap(a, b);
+	std::cout << "A: " << a << " B: " << b << std::endl;
+
+	std::cout << "C: " << c << " D: " << d << std::endl;
+	swap(c, d);
+	std::cout << "C: " << c << " D: " << d << std::endl;
+
+	std::string	y = "HOLA", z = "MUNDO";
+
+	std::cout << "Y: " << y << " Z: " << z << std::endl;
+	swap(y, z);
+	std::cout << "Y: " << y << " Z: " << z << std::endl;
+	return (0);
+}
---
+++ cpp-07/ex00/whatever.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molasz-a <molasz-a@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 17:38:51 by molasz-a          #+#    #+#             */
+/*   Updated: 2024/10/15 17:55:03 by molasz-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T> void	swap(T &x, T &y)
+{
+	T	temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T> T	min(T x, T y)
+{
+	if (x > y)
+		return y;
+	return x;
+}
+
+template <typename T> T	max(T x, T y)
+{
+	if (x > y)
+		return x;
+	return y;
+}
