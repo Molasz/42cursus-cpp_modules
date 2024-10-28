1cfee474257f714c27ac7f75b64b772e5c287061
Molasz <martiolasz@gmail.com>
2024-10-28 20:02:01 +0100

    ex00 done

--- ex08/ex00/easyfind.hpp
+++ ex08/ex00/easyfind.hpp
@@ -6,11 +6,33 @@
 /*   By: molasz-a <molasz-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:55:27 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/10/28 19:13:25 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/10/28 19:59:25 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
-#include <algorithm>
+#include <cstdlib>
+#include <vector>
+#include <deque>
+#include <list>
+
+class notFoundException: public std::exception
+{
+  virtual const char* what() const throw()
+  {
+    return "Not found";
+  }
+} myex;
+
+template <typename T>
+int	easyfind(T c, int n)
+{
+	for (typename T::iterator i = c.begin(); i != c.end(); i++)
+	{
+		if (*i == n)
+			return (n);
+	}
+	throw notFoundException();
+}
--- ex08/ex00/main.cpp
+++ ex08/ex00/main.cpp
@@ -6,13 +6,34 @@
 /*   By: molasz-a <molasz-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:58:02 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/10/28 19:13:40 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/10/28 20:00:03 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+#define SIZE 10
+
 int	main(void)
 {
+	std::deque<int>	items;
+	int	n;
+	
+	srand(time(0));
+
+	for (int i = 0; i < SIZE; i++)
+		items.push_back(i);
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		try
+		{
+			n = rand() % SIZE * 2 + 1;
+			easyfind(items, n);
+			std::cout << n << " found in container" << std::endl;
+		} catch (std::exception err) {
+			std::cout << n << " not found in container" << std::endl;
+		}
+	}
 	return (0);
 }
